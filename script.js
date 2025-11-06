18d4d1650cc92b79df4df6eb68fa65582ea5fe55
Your Name <you@example.com>
2025-11-06 10:07:45 +0530

    updated-responsively

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 let selectedRow;
 
-// Function Add
 function addData() {
-
-    let name = document.getElementById('name').value.trim();
-    let age = document.getElementById('age').value.trim();
-    let salary = document.getElementById('salary').value.trim();
-    let department = document.getElementById('department').value.trim();
-    let table = document.getElementById('tableData');
+    let name = document.getElementById("name").value;
+    let age = document.getElementById("age").value;
+    let salary = document.getElementById("salary").value;
+    let department = document.getElementById("department").value;
+    let table = document.getElementById("tableData");
 
     if (!name || !age || !salary || !department) {
-        alert("Need to fill all the fields");
+        alert("Fill all the fields")
     }
     else {
         let row = table.insertRow();
@@ -19,57 +17,47 @@ function addData() {
         row.insertCell(2).textContent = salary;
         row.insertCell(3).textContent = department;
 
-        row.insertCell(4).innerHTML = ` <button type="submit" onclick="editData(this)">Edit</button>
-    <button type="submit" onclick="deleteData(this)">Delete</button> `
-        clearField();
+        row.insertCell(4).innerHTML = `<button onclick="edit(this)">Edit</button>
+        <button onclick="del(this)">Delete</button>`;
+
+        clearAll();
     }
-
 }
 
-// Delete Row
-
-function deleteData(button) {
-
-    let row = button.parentElement.parentElement;
-    let table = document.getElementById('tableData');
-    table.deleteRow(row.rowIndex);
-
-    clearField();
-
-}
-
-// Edit Row
-
-function editData(button) {
+function edit(button){
     selectedRow = button.parentElement.parentElement;
+    let btn = document.getElementById("update");
 
-    document.getElementById('name').value = selectedRow.cells[0].textContent;
-    document.getElementById('age').value = selectedRow.cells[1].textContent;
-    document.getElementById('salary').value = selectedRow.cells[2].textContent;
-    document.getElementById('department').value = selectedRow.cells[3].textContent;
+    document.getElementById("name").value=selectedRow.cells[0].textContent;
+    document.getElementById("age").value=selectedRow.cells[1].textContent;
+    document.getElementById("salary").value=selectedRow.cells[2].textContent;
+    document.getElementById("department").value=selectedRow.cells[3].textContent;
 
-    let updateButton = document.getElementById('update');
-    updateButton.innerHTML = `<button onclick="updateData()">Update</button> `;
+    btn.innerHTML = `<button onclick="update()">Update</button>`;
+}
+
+function update(){
+    selectedRow.cells[0].textContent = document.getElementById("name").value ;
+    selectedRow.cells[1].textContent = document.getElementById("age").value ;
+    selectedRow.cells[2].textContent = document.getElementById("salary").value ;
+    selectedRow.cells[3].textContent = document.getElementById("department").value ;
+
+    document.getElementById("update").innerHTML = `<button type="submit" onclick="addData()">Submit</button>`;
+    clearAll();
+    alert("Record Updated Successfully")
 
 }
 
-function updateData() {
-
-    selectedRow.cells[0].textContent = document.getElementById('name').value;
-    selectedRow.cells[1].textContent = document.getElementById('age').value;
-    selectedRow.cells[2].textContent = document.getElementById('salary').value;
-    selectedRow.cells[3].textContent = document.getElementById('department').value;
-
-    let updateButton = document.getElementById('update');
-    updateButton.innerHTML = `<button onclick="addData()">Submit</button> `;
-    clearField()
+function del(button){
+    let row = button.parentElement.parentElement;
+    row.remove();
+    alert("Record Deleted")
 }
 
-// Function Clear Field
+function clearAll(){
+    document.getElementById("name").value="";
+    document.getElementById("age").value="";
+    document.getElementById("salary").value="";
+    document.getElementById("department").value="";
 
-function clearField() {
-    document.getElementById('name').value = "";
-    document.getElementById('age').value = "";
-    document.getElementById('salary').value = "";
-    document.getElementById('department').value = "";
 }
